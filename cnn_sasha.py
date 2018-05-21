b01059574d69e8e535f9e1daab41f3483c7f8368
Guillaume Germain <g.germain@me.com>
2018-05-21 23:53:36 +0200

    CNN Sasha cleanup
    easier and cleaner configuration for several users calling the script

--- cnn_sasha.py
+++ cnn_sasha.py
@@ -1,27 +1,11 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Mon May 21 20:55:27 2018
-
 @author: guillaume
-
 New script for Sasha
-
 """
 
-
-# Convolutional Neural Network
-
-# Part 1 - Building the CNN
-
-# Importing the Keras libraries and packages
-#from keras.models import Sequential
-#from keras.layers import Convolution2D
-#from keras.layers import MaxPooling2D
-#from keras.layers import Flatten
-#from keras.layers import Dense
-
-from cnn_util import print_file_layers, train_classifier
+import numpy as np
+from cnn_util import train_classifier #, print_file_layers
 
 
 filename = "classifier_1_epoch_45.h5"
@@ -37,31 +21,53 @@ model.summary()
 
 
 # train the model for a few epochs and check if it is improving
+CONFIG = "GPU"
 
-# config Guillaume CPU
-#EPOCH_0 = 45
-#EPOCHS = 10
-#histody = train_classifier(model, new_epochs=10, batch_size=16, verbose=1,
-#                 initial_epoch=EPOCH_0,
-#                 use_checkpoints=True, train_size=2000, test_size=500)
 
-# config Sasha GPU
-EPOCH_0 = 45
-EPOCHS = 50
-train_classifier(model, new_epochs=47, batch_size=16, verbose=1,
+if CONFIG == "CPU":
+    # config Guillaume CPU
+    EPOCH_0 = 45
+    EPOCHS = 10
+    TRAIN_SIZE = 2000
+    TEST_SIZE = 500
+    BATCH_SIZE = 16
+else:
+    # config Sasha GPU
+    EPOCH_0 = 45
+    EPOCHS = 50
+    TRAIN_SIZE = 8000
+    TEST_SIZE = 2000
+    BATCH_SIZE = 32 #or 64
+
+
+history = train_classifier(model, new_epochs=10, batch_size=BATCH_SIZE, verbose=1,
                  initial_epoch=EPOCH_0,
-                 use_checkpoints=True, train_size=8000, test_size=2000)
+                 use_checkpoints=True, train_size=TRAIN_SIZE, test_size=TEST_SIZE)
 
 
 #Save model once training is done
 model.save("classifier_1_epoch_" + str(EPOCH_0 + EPOCHS) + ".h5")
 
 #TODO Save history for plotting
+loss_history = history.history["loss", "acc"]
+np_loss_history = np.array(loss_history)
+np.savetxt("loss_history.txt", np_loss_history, delimiter=",")
 
 
+
+
+
+# Further CNN extension
+
+# Importing the Keras libraries and packages
+#from keras.models import Sequential
+#from keras.layers import Convolution2D
+#from keras.layers import MaxPooling2D
+#from keras.layers import Flatten
+#from keras.layers import Dense
 
 
 #Take current model, add dropout, train over 50 epochs and check results
-
+#model_2 = model
 
 #Take current model, add one intermdiate convolution block
